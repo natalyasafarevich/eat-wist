aae1616b10586cda9470fb4615ec542be730fe7c
Natalya <natalyasafarevich@gmail.com>
2023-08-16 13:31:05 +0200

    data types for main page are added

--- src/DataTypeCard/DataTypeCard.tsx
+++ src/DataTypeCard/DataTypeCard.tsx
@@ -1,21 +1,25 @@
 import React, { FC, useState } from "react";
-import "./DataTypeCard";
 import dataType from "../moduls/dafaultValue";
 import { Link } from "react-router-dom";
+import "./DataTypeCard.scss";
+import { combineWords } from "../helper/concat";
 
 const DataTypeCard: FC = () => {
-	const [isMove, setIsMove] = useState<boolean>(false);
 
 	return (
 		<div className="data-card">
 			<div className="data-card__wrap">
-				{dataType.map((type) => {
+				{dataType.map((type, i) => {
 					return (
-						<div key={type.name} className="data-card__box">
+						<div key={type.name} className={`data-card__box data-card__box_${i}`}>
 							<div className="data-card__content">
 								<div className="data-card__info">
 									<p className="data-card__title">{type.name}</p>
-									<Link className="data-card__link" to={`/datatype/${type.name}`}>see more</Link>
+									<Link
+										className="data-card__link button-green"
+										to={`/datatype/${combineWords(type.name)}`}>
+										see more
+									</Link>
 								</div>
 							</div>
 						</div>
